67b751e7750daef6c2e20a8d4c0c7d76baa8d373
binmek94th <binaymm83@gmail.com>
2023-12-29 08:53:52 +0300

    Budget Model created

---
+++ TrackWallet/TrackWallet.Models/Budget.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrackWallet.Models;
+
+public class Budget
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [ForeignKey("ApplicationUser")]
+    public string UserId { get; set; }
+    public ApplicationUser ApplicationUser { get; set; }
+    
+    [ForeignKey("Category")] 
+    public int id { get; set; }
+    public Category Category { get; set; }
+    
+    [Required]
+    public string BudgetType { get; set; }
+    [Required]
+    public double Amount { get; set; }
+}
