1d4195018b9890ea6c4c601ecc3bf30177601c4f
binmek94th <binaymm83@gmail.com>
2024-01-12 23:19:09 +0300

    Unversioned

---
+++ TrackWallet/TrackWallet.DataAccess/Migrations/20240112201751_sharedWallet.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackWallet.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class sharedWallet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SharedWallets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    WalletId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SharedWallets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SharedWallets_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SharedWallets_Wallets_WalletId",
+                        column: x => x.WalletId,
+                        principalTable: "Wallets",
+                        principalColumn: "WalletId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedWallets_UserId",
+                table: "SharedWallets",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedWallets_WalletId",
+                table: "SharedWallets",
+                column: "WalletId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SharedWallets");
+        }
+    }
+}
